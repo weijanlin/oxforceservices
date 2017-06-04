0f9c53c77b433c576370848cb22bc0a011a5b7e9
HardRoot <hard25670559@gmail.com>
2017-06-05 03:54:49 +0800

    WindowsForm Lab1版本 + gitignore測試
    WindowsForm Lab1版本 + gitignore測試

---
+++ lab1-WindowsForm/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace lab1_WindowsForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pageSetupDialog1 = new System.Windows.Forms.PageSetupDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.id = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.age = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.sex = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.bear = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.emotion = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(139, 609);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(148, 43);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "請選擇圖片";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.id,
+            this.age,
+            this.sex,
+            this.bear,
+            this.emotion});
+            this.listView1.Location = new System.Drawing.Point(413, 601);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(583, 116);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // id
+            // 
+            this.id.Text = "ID";
+            this.id.Width = 400;
+            // 
+            // age
+            // 
+            this.age.Text = "年齡";
+            // 
+            // sex
+            // 
+            this.sex.Text = "性別";
+            // 
+            // bear
+            // 
+            this.bear.Text = "鬍子";
+            // 
+            // emotion
+            // 
+            this.emotion.Text = "情緒";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 47);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(984, 548);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(12, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("新細明體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.Location = new System.Drawing.Point(421, 294);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(154, 24);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "請先選擇圖片";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1008, 729);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PageSetupDialog pageSetupDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ColumnHeader id;
+        private System.Windows.Forms.ColumnHeader age;
+        private System.Windows.Forms.ColumnHeader sex;
+        private System.Windows.Forms.ColumnHeader bear;
+        private System.Windows.Forms.ColumnHeader emotion;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ lab1-WindowsForm/Form1.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.ProjectOxford.Face;
+using Microsoft.ProjectOxford.Face.Contract;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab1_WindowsForm
+{
+    public partial class Form1 : Form
+    {
+
+        private const string faceApi_Key = "33413afb703240b18fc1ff941093b4b2";                  //KEY
+        private IFaceServiceClient faceServiceClient = new FaceServiceClient(faceApi_Key);      //
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            string imgPath = null;
+            try
+            {
+                imgPath = this.GetImageFilePath();
+                this.label1.Text = imgPath;
+            }
+            catch(ArgumentNullException)
+            {
+                return;
+            }
+
+            this.DrowImg(imgPath);
+            this.label2.Visible = false;
+
+            Face[] facesInfo = await GetFacesInfo(imgPath, FaceAttributeType.Age, FaceAttributeType.Gender, FaceAttributeType.FacialHair, FaceAttributeType.Emotion);
+            this.label1.Text = "找到了" + facesInfo.Length + "張臉";
+
+            this.ShowData(facesInfo);
+
+        }
+
+        private void ShowData(Face[] facesInfo)
+        {
+            foreach (var tmp in facesInfo)
+            {
+                ListViewItem item = new ListViewItem(tmp.FaceId.ToString());
+                item.SubItems.Add("" + tmp.FaceAttributes.Age);
+                item.SubItems.Add("" + tmp.FaceAttributes.Gender == "male" ? "男" : "女");
+                item.SubItems.Add("" + tmp.FaceAttributes.FacialHair.Moustache);
+                item.SubItems.Add("" + tmp.FaceAttributes.Emotion.Anger);
+                this.listView1.Items.Add(item);
+            }
+        }
+
+        private async Task<Face[]> GetFacesInfo(string path, params FaceAttributeType[] attribute)
+        {
+            try
+            {
+                using (Stream imageFileStream = File.OpenRead(path))
+                {
+                    var facesInfo = await this.faceServiceClient.DetectAsync(imageFileStream, returnFaceId: true, returnFaceAttributes: attribute);
+                    return facesInfo;
+                }
+            }
+            catch(Exception)
+            {
+                return new Face[0];
+            }
+        }
+
+        private void DrowImg(string path)
+        {
+            Bitmap img = new Bitmap(path);
+            pictureBox1.Image = img;
+        }
+
+        private string GetImageFilePath()
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.Filter = "JPEG Image(*.jpg)|*.jpg";
+            if (fileDialog.ShowDialog(this) != DialogResult.OK)
+            {
+                throw new ArgumentNullException();
+            }
+                
+            return fileDialog.FileName;
+        }
+
+    }
+}
